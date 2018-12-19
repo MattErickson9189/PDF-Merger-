57048715e86fa21a8dd2dcc7802b66bba22bb276
Matt Erickson <matterickson9819@gmail.com>
2018-12-19 17:34:20 -0600

    Scans through all PDFS

--- combine.py
+++ combine.py
@@ -1,3 +1,30 @@
 #Python 3 PDF combiner
 
+import os, PyPDF2, sys
 
+
+path = sys.argv[1]
+pagesScanned = 0;
+
+pdfFiles = []
+
+for filename in os.listdir(path):
+    if filename.endswith('.pdf'):
+        pdfFiles.append(filename)
+
+pdfFiles.sort()
+
+pdfWriter = PyPDF2.PdfFileWriter()
+
+for filename in pdfFiles:
+    pdfFileObj = open(os.path.join(path,filename), 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    for pageNum in range (0, pdfReader.numPages):
+        pageObj = pdfReader.getPage(pageNum)
+        pdfWriter.addPage(pageObj)
+        pagesScanned = pagesScanned+1
+        print(pagesScanned)
+
+pdfOutput = open('mergedPDF.pdf', 'wb')
+pdfWriter.write(pdfOutput)
+pdfOutput.close()
